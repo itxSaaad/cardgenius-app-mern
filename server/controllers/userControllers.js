d91301251b266073ca77bbcac55dd2877f26e1b7
itxSaaad <saadstudent.cs@gmail.com>
2023-07-17 13:14:20 +0500

    Added: Admin & Protected User Routes

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -29,6 +29,7 @@ const authUser = asyncHandler(async (req, res) => {
             email: user.email,
             isAdmin: user.isAdmin,
             token: generateToken(user._id),
+            message: 'Login Successful!',
           });
         } else {
           res.status(401);
@@ -83,6 +84,7 @@ const registerUser = asyncHandler(async (req, res) => {
               email: user.email,
               isAdmin: user.isAdmin,
               token: generateToken(user._id),
+              message: 'User Registered Successfully!',
             });
           } else {
             res.status(400);
@@ -97,4 +99,182 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { authUser, registerUser };
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      message: 'User Profile Fetched Successfully!',
+    });
+  } else {
+    res.status(404);
+    throw new Error('User Not Found!');
+  }
+});
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    let { name, email, password, confirmPassword } = req.body;
+
+    if (!name || !email || !password || !confirmPassword) {
+      res.status(400);
+      throw new Error('All Fields Are Required!');
+    } else {
+      if (emailValidator.validate(req.body.email)) {
+        const match = await bcrypt.compare(password, user.password);
+
+        if (user.email === email && match) {
+          res.status(400);
+          throw new Error('You Have Not Made Any Changes!');
+        } else {
+          if (password !== confirmPassword) {
+            res.status(400);
+            throw new Error('Passwords Do Not Match!');
+          } else {
+            const salt = await bcrypt.genSalt(Number(process.env.SALT));
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            user.name = name || user.name;
+            user.email = email || user.email;
+            user.password = hashedPassword || user.password;
+
+            const updatedUser = await user.save();
+
+            res.json({
+              _id: updatedUser._id,
+              name: updatedUser.name,
+              email: updatedUser.email,
+              isAdmin: updatedUser.isAdmin,
+              token: generateToken(updatedUser._id),
+              message: 'User Profile Updated Successfully!',
+            });
+          }
+        }
+      } else {
+        res.status(400);
+        throw new Error('Invalid Email Address!');
+      }
+    }
+  } else {
+    res.status(404);
+    throw new Error('User Not Found!');
+  }
+});
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+
+  if (users) {
+    res.json(users);
+  } else {
+    res.status(404);
+    throw new Error('Users Not Found!');
+  }
+});
+
+// @desc    Delete a user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+
+    res.json({ message: 'User Removed Successfully!' });
+  } else {
+    res.status(404);
+    throw new Error('User Not Found!');
+  }
+});
+
+// @desc    Get user by ID
+// @route   GET /api/users/profile/:id
+// @access  Private/Admin
+
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User Not Found!');
+  }
+});
+
+// @desc    Update user
+// @route   PUT /api/users/profile/:id
+// @access  Private/Admin
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    let { name, email, isAdmin } = req.body;
+
+    if (!name || !email) {
+      res.status(400);
+      throw new Error('All Fields Are Required!');
+    } else {
+      if (emailValidator.validate(req.body.email)) {
+        const match = await bcrypt.compare(password, user.password);
+
+        if (user.email === email && match) {
+          res.status(400);
+          throw new Error('You Have Not Made Any Changes!');
+        } else {
+          user.name = name || user.name;
+          user.email = email || user.email;
+          user.isAdmin = isAdmin;
+
+          const updatedUser = await user.save();
+
+          res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            message: 'User Updated Successfully!',
+          });
+        }
+      } else {
+        res.status(400);
+        throw new Error('Invalid Email Address!');
+      }
+    }
+  } else {
+    res.status(404);
+    throw new Error('User Not Found!');
+  }
+});
+
+module.exports = {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getAllUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-const { authUser, registerUser } = require('../controllers/userControllers');
+const {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getAllUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} = require('../controllers/userControllers');
 
 const { protect, admin } = require('../middlewares/authMiddleware');
 
+router.route('/register').post(registerUser);
 router.post('/login', authUser);
 
-router.post('/register', registerUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+
+router.route('/').get(protect, admin, getAllUsers);
+router.route('/:id').delete(protect, admin, deleteUser);
+router
+  .route('/profile/:id')
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 module.exports = router;
