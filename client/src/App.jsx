83a2ef5bd27b398c2fe626180f9770a9245badf6
itxSaaad <saadstudent.cs@gmail.com>
2023-07-28 15:46:19 +0500

    Updated: React Router Dom Functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,13 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
-import Loader from './components/ui/Loader';
-
-const HomeScreen = React.lazy(() => import('./screens/HomeScreen'));
-const GenerateCardScreen = React.lazy(() =>
-  import('./screens/GenerateCardScreen')
-);
-const ContactScreen = React.lazy(() => import('./screens/ContactScreen'));
-const ProfileScreen = React.lazy(() => import('./screens/ProfileScreen'));
 
 function App() {
   return (
-    <BrowserRouter>
-      <Suspense
-        fallback={
-          <div className="min-h-screen flex justify-center items-center">
-            <Loader />
-          </div>
-        }
-      >
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/generate-card" element={<GenerateCardScreen />} />
-            <Route path="/contact-us" element={<ContactScreen />} />
-            <Route path="/profile" element={<ProfileScreen />} />
-          </Routes>
-        </Layout>
-      </Suspense>
-    </BrowserRouter>
+    <Layout>
+      <Outlet />
+    </Layout>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 
-import store from './redux/store.js';
 import App from './App.jsx';
 import './index.css';
 
+import store from './redux/store.js';
+
+import Loader from './components/ui/Loader.jsx';
+
+const HomeScreen = React.lazy(() => import('./screens/HomeScreen.jsx'));
+const GenerateCardScreen = React.lazy(() =>
+  import('./screens/GenerateCardScreen.jsx')
+);
+const ContactScreen = React.lazy(() => import('./screens/ContactScreen.jsx'));
+const ProfileScreen = React.lazy(() => import('./screens/ProfileScreen.jsx'));
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/generate-card" element={<GenerateCardScreen />} />
+      <Route path="/contact-us" element={<ContactScreen />} />
+      <Route path="/profile" element={<ProfileScreen />} />
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <Suspense
+        fallback={
+          <div className="min-h-screen flex justify-center items-center">
+            <Loader />
+          </div>
+        }
+      >
+        <RouterProvider router={router} />
+      </Suspense>
+    </React.StrictMode>
+  </Provider>
 );
